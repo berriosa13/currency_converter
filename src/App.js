be69c9b569fe5b72eb41c7d824bbfcb24ab71f26
berriosa13 <berriosa13@gmail.com>
2021-05-27 13:48:07 -0400

    first project commit
    currency converter using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import CurrencyRow from './CurrencyRow';
 import './App.css';
 
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=7c5b72f62efad96ce822c19044321e2d'; 
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCuurrency, setFromCurrency] = useState()
+  const [toCuurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState() 
+  const [amount,setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  /*
+  useEffect reads in an empty array as second parameter
+  so that useEffect() is only ran once when the app loads
+  */ 
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <h1> Currency Converter </h1>
+        <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCuurrency} 
+        onChangeCurrency = {e => setFromCurrency(e.target.value)}
+         />
+        <div className="equals"> = </div>
+        <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCuurrency}
+        onChangeCurrency = {e => setToCurrency(e.target.value)}
+        />
+      </>
+    
   );
 }
 
